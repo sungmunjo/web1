d05bf7c4156b15c4904503a0e39d6569f98a52a2
sungmunjo <moonjo960@gmail.com>
2021-07-19 16:02:19 +0900

    html_javascript

--- color.js
+++ color.js
@@ -1,9 +1,24 @@
+var link = {
+  setcolor:function(color){
+
+    // var alist = document.querySelectorAll('a');
+    // var i=0;
+    // while(i<alist.length){
+    //   alist[i].style.color = color;
+    //   i = i+1;
+    // }
+    $('a').css("color", color);
+  }
+}
+
 var body = {
   setcolor:function(color){
-    document.querySelector('body').style.color = color;
+  //  document.querySelector('body').style.color = color;
+    $('body').css("color",color);
   },
   setBackgroundColor:function(color){
-    document.querySelector('body').style.backgroundColor = color;
+  //  document.querySelector('body').style.backgroundColor = color;
+    $('body').css("backgroundColor",color);
   }
 }
 function night_day_handler(self){
@@ -11,23 +26,16 @@ function night_day_handler(self){
   var i =0;
   var alist = document.querySelectorAll('a');
   if(self.value === 'Night'){
-    target.style.backgroundColor = 'black';
-    target.style.color = 'white';
     self.value = 'Day';
-
-    while(i<alist.length){
-      alist[i].style.color = 'red';
-      i = i+1;
-    }
+    body.setcolor("white");
+    body.setBackgroundColor("black");
+    link.setcolor("white");
   }
   else{
-    target.style.backgroundColor = 'white';
-    target.style.color = 'black';
+    body.setcolor("black");
+    body.setBackgroundColor("white");
     self.value = 'Night';
 
-    while(i<alist.length){
-      alist[i].style.color = 'blue';
-      i = i+1;
-    }
+    link.setcolor("blue");
   }
 }
